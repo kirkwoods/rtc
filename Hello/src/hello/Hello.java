cd18d5b66cfd48f739cdf022d3bbb8219d757dfc
kmwoods <kmwoods@9.80.86.118>
2017-09-12 11:17:43 -0500

    [2602] Say Hello to the world.

---
+++ Hello/src/hello/Hello.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package hello;
+
+/**
+ * @author kmwoods
+ *
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World!");
+
+	}
+
+}
